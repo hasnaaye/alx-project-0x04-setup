16fa56419e43c1ea70ae824df29c01d44d335d36
Hasnaa Yerrou <hasnaeyerrou@gmail.com>
2025-06-11 00:40:17 +0100

    initial commit

---
+++ alx-project-0x06/interface/index.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "raect";
+
+interface ButtonProps {
+  buttonLabel: string
+  buttonSize?: string
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+  action?: () => void
+}
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+interface PageRouteProps {
+  pageRoute: string
+}
